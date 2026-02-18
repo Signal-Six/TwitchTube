fac8c5a1e57057b3320c161cda4a292870f1d15c
Signal-Six <pentiumiibrain@proton.me>
2026-02-18 14:59:02 +0000

    Log Out Redirects Back to Main Page

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,5 +3,5 @@ import { logout } from '@/lib/auth';
 
 export async function GET() {
   logout();
-  return NextResponse.redirect('http://localhost:3000/');
+  return NextResponse.redirect('https://twitch-tube.vercel.app/');
 }
